89f2cb53ba5eefcd45828902ee4247c88ccc8609
Ahmed Rahem <ahmadrahem1798@gmail.com>
2021-05-09 11:22:47 +0300

    initital commit

---
+++ fun.py
@@ -0,0 +1,7 @@
+def hello(x):
+    print('Hi ' + str(x))
+    print('my name')
+    print('Hello there')
+hello(3)
+#hello()
+#hello()
---
+++ fun5.py
@@ -0,0 +1,9 @@
+def spam(divideby):
+    try:
+      return 42 / divideby
+    except ZeroDivisionError:
+        print('try another arrgument')
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
